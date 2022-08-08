8d20a1636a33e7cba89fd00d88bc5a3ed7a82b3e
deepak <dg91918@gmail.com>
2022-08-08 16:39:41 +0530

    frst

---
+++ deploy.js
@@ -0,0 +1,21 @@
+const HDWalletProvider = require("truffle-hdwallet-provider");
+const Web3 = require("web3");
+const { interface, bytecode } = require("./compile");
+
+const provider = new HDWalletProvider(
+  "harbor install escape symptom phrase extend evolve cause clinic puzzle claw era",
+  "https://rinkeby.infura.io/v3/81163e6c950b411b97443686604f6112"
+);
+
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+  const accounts = await web3.eth.getAccounts();
+  const res = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({ data: "0x" + bytecode })
+    .send({ from: accounts[0], gas: "1000000", gasPrice: "2000000000" });
+
+  console.log(interface);
+  console.log("contract deployed to ", res.options.address);
+};
+deploy();
